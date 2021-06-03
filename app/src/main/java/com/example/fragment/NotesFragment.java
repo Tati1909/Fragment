a8a4ba2575988a2badfdd596eca525a3e639c652
татьяна <emuart155@gmail.com>
2021-06-03 15:17:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fragment/NotesFragment.java
@@ -0,0 +1,14 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NotesFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
