042fadd035919acbe5ea4cb3ecb6f7589cb2784f
татьяна <emuart155@gmail.com>
2021-06-11 14:44:01 +0300

    некоторые доработки, намудрила с ветками

--- app/src/main/java/com/example/fragment/NotesEntity.java
+++ app/src/main/java/com/example/fragment/NotesEntity.java
@@ -2,31 +2,30 @@ package com.example.fragment;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-
-public class DossierEntity implements Parcelable {
+public class NotesEntity implements Parcelable {
     public String title;
     public String description;
     public String date;
 
-    public DossierEntity(String title, String description, String date) {
+    public static final Creator<NotesEntity> CREATOR = new Creator<NotesEntity>() {
+        @Override
+        public NotesEntity createFromParcel(Parcel in) {
+            return new NotesEntity(in);
+        }
+
+        @Override
+        public NotesEntity[] newArray(int size) {
+            return new NotesEntity[size];
+        }
+    };
+
+    public NotesEntity(String title, String description, String date) {
         this.title = title;
         this.description = description;
         this.date = date;
     }
 
-    public static final Creator<DossierEntity> CREATOR = new Creator<DossierEntity>() {
-        @Override
-        public DossierEntity createFromParcel(Parcel in) {
-            return new DossierEntity(in);
-        }
-
-        @Override
-        public DossierEntity[] newArray(int size) {
-            return new DossierEntity[size];
-        }
-    };
-
-    protected DossierEntity(Parcel in) {
+    protected NotesEntity(Parcel in) {
         title = in.readString();
         description = in.readString();
         date = in.readString();
